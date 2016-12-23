1be86281d22b4ab8bc4a45c82685e4f261ea321a
Daniel Ireson <daniel@djgraphics.co.uk>
2016-12-23 19:37:03 +0000

    add url path to app view

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react'
 import Clock from './components/Clock'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      customPath: ''
+    }
+  }
+
   render() {
+    const styles = {
+      url: {
+        fontWeight: '300'
+      }
+    }
+
+    let url = window.location.protocol + '//' + window.location.host + '/' + this.state.customPath
     return (
-      <Clock />
+      <div>
+        <h1 style={styles.url}>{ url }</h1>
+        <Clock />
+      </div>
     )
   }
 }
