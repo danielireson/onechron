fa93f4c6a418d052cb4d54e56bfb9351aab1cbad
Daniel Ireson <daniel@djgraphics.co.uk>
2017-01-01 15:11:19 +0000

    remove not needed firebase config variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,10 +2,7 @@ import firebase from 'firebase'
 
 const config = {  
   apiKey: process.env.REACT_APP_FB_API_KEY,
-  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FB_DATABASE_URL,
-  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FB_SENDER_ID,
 }
 
 firebase.initializeApp(config)
