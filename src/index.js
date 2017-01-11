12f414e9583968b8f786711d0a540e0f38f50ba3
Daniel Ireson <daniel@djgraphics.co.uk>
2017-01-11 21:57:23 +0000

    set background colour on load using js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { COLOURS } from './config/vars.js'
 import './config/firebase'
 import './index.css'
+
+document.documentElement.style.backgroundColor = COLOURS.BLUE
 
 ReactDOM.render(
   <App />,
