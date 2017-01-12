84c9802c7b5b535848fef78f95e400542a513fdf
Daniel Ireson <daniel@djgraphics.co.uk>
2017-01-12 16:41:42 +0000

    add tooltip to timer link

--- src/components/TimerLink.js
+++ src/components/TimerLink.js
@@ -1,33 +1,66 @@
 import React, { Component } from 'react'
 import Radium from 'radium'
 import copy from 'copy-to-clipboard'
-import { SIZE } from '../config/vars'
+import { COLOURS, SIZE } from '../config/vars'
 
 class TimerLink extends Component {
   constructor(props) {
     super(props)
     this.baseUrl = window.location.protocol + '//' + window.location.host + '/'
     this.handleClick = this.handleClick.bind(this)
+    this.onMouseEnter = this.onMouseEnter.bind(this)
+    this.onMouseLeave = this.onMouseLeave.bind(this)
   }
 
   handleClick() {
     copy(this.state.url)
   }
 
+  onMouseEnter(event) {
+    if (event.target.tagName === 'H1') {
+      event.target.children[0].style.display = 'block'
+    }
+  }
+
+  onMouseLeave(event) {
+    if (event.target.tagName === 'H1') {
+      event.target.children[0].style.display = 'none'
+    }
+  }
+
   render() {
     const styles = {
       url: {
+        display: 'inline-block',
         fontWeight: '300',
         marginBottom: SIZE.px(4),
+        position: 'relative',
         userSelect: 'none',
         ':hover': {
           cursor: 'copy',
         },
       },
+      tooltip: {
+        background: COLOURS.DARK_BLUE,
+        borderRadius: SIZE.px(1),
+        color: COLOURS.WHITE,
+        display: 'none',
+        fontSize: '50%',
+        paddingTop: SIZE.px(2),
+        paddingBottom: SIZE.px(2),
+        position: 'absolute',
+        top: '-' + SIZE.em(3),
+        left: 0,
+        width: '100%',
+        zIndex: '100',
+      },
     }
 
     return (
-      <h1 onClick={this.handleClick} style={styles.url}>{this.baseUrl + this.props.path}</h1>
+      <h1 onClick={this.handleClick} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} style={styles.url}>
+        <span style={styles.tooltip}>Copy to clipboard</span>
+        {this.baseUrl + this.props.path}
+      </h1>
     )
   }
 }
