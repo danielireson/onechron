71df46bc25cc2384e30406b57eed26a52764fdc9
Daniel Ireson <daniel@djgraphics.co.uk>
2017-01-12 15:42:55 +0000

    generate base css through Radium at App component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react'
 import { Router, Route, browserHistory } from 'react-router'
+import { Style } from 'radium'
+import { COLOURS, SIZE } from './config/vars.js'
 import Home from './pages/Home'
 import Live from './pages/Live'
 
 class App extends Component {
   render() {
+    const styles = {
+      html: {
+        backgroundColor: COLOURS.BLUE,
+      },
+      body: {
+        color: COLOURS.WHITE,
+        fontFamily: 'sans-serif',
+        marginBottom: SIZE.px(20),
+        marginLeft: 0,
+        marginRight: 0,
+        marginTop: SIZE.px(20),
+        padding: 0,
+        textAlign: 'center',
+      },
+      a: {
+        color: COLOURS.WHITE,
+        textDecoration: 'none',
+      }
+    }
+
     return (
-      <Router history={browserHistory}>
-        <Route path='/' component={Home} />
-        <Route path='/:path' component={Live} />
-      </Router>
+      <div>
+        <Style rules={styles} />
+        <Router history={browserHistory}>
+          <Route path='/' component={Home} />
+          <Route path='/:path' component={Live} />
+        </Router>
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { COLOURS } from './config/vars.js'
 import './config/firebase'
-import './index.css'
-
-document.documentElement.style.backgroundColor = COLOURS.BLUE
 
 ReactDOM.render(
   <App />,
