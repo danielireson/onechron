bc566d83cee393db7c6aacd2bace0466ac831b26
Daniel Ireson <daniel@djgraphics.co.uk>
2017-02-22 21:12:09 +0000

    update custom time button to use custom input over window alert

--- src/components/TimerControls.js
+++ src/components/TimerControls.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react'
 import Radium from 'radium'
-import { SIZE } from '../config/vars.js'
+import { BP, COLOURS, SIZE } from '../config/vars.js'
 import Button from './Button'
 
 class TimerControls extends Component {
-  getCustomInput() {
-    let input = parseInt(prompt('Minutes to count down from'), 10)
-    if (Number.isInteger(input)) {
-      this.props.setTime(input)
+  constructor() {
+    super()
+    this.state = {
+      hasCustomTimeControls: false
+    }
+    this.setCustomTime = this.setCustomTime.bind(this)
+    this.toggleCustomTimeControls = this.toggleCustomTimeControls.bind(this)
+  }
+
+  toggleCustomTimeControls() {
+    if (this.state.hasCustomTimeControls) {
+      this.setState({
+        hasCustomTimeControls: false
+      })
+    } else {
+      this.setState({
+        hasCustomTimeControls: true
+      }, () => {
+        document.getElementById('custom-time-input').focus()
+      })      
+    }
+    let input = document.getElementById('custom-time-input')
+    input.value = ''
+  }
+
+  setCustomTime() {
+    let input = document.getElementById('custom-time-input')
+    let time = Number(input.value)
+    if (Number.isInteger(time)) {
+      this.props.setTime(time)
+      this.toggleCustomTimeControls()
+    }
+  }
+
+  getCustomTimeControlsDisplay() {
+    if (this.state.hasCustomTimeControls) {
+      return 'block'
+    } else {
+      return 'none'
     }
   }
 
@@ -20,7 +55,27 @@ class TimerControls extends Component {
         fontSize: SIZE.em(1),
         textTransform: 'uppercase',
         marginTop: 0,
-        marginBottom: SIZE.px(2)
+        marginBottom: SIZE.px(2),
+      },
+      customTimeControls: {
+        display: this.getCustomTimeControlsDisplay()
+      },
+      input: {
+        border: 'none',
+        boxSizing: 'border-box',
+        color: COLOURS.BLUE,
+        fontSize: SIZE.em(1),
+        marginBottom: SIZE.px(2),
+        outline: 'none',
+        padding: SIZE.px(2),
+        textAlign: 'center',
+        width: '100%',
+        [BP.MEDIUM]: {
+          borderRadius: SIZE.px(1),
+          marginBottom: 0,
+          marginRight: SIZE.px(1),
+          width: SIZE.px(15),
+        },
       }
     }
 
@@ -39,12 +94,18 @@ class TimerControls extends Component {
               <Button icon='clock-o' text='10m'  onClick={() => this.props.setTime(10)} />
               <Button icon='clock-o' text='15m'  onClick={() => this.props.setTime(15)} />
               <Button icon='clock-o' text='20m'  onClick={() => this.props.setTime(20)} />
-              <Button icon='clock-o' text='Custom' onClick={this.getCustomInput.bind(this)} />
+              <Button icon='clock-o' text='Custom' onClick={this.toggleCustomTimeControls} />
               <Button icon='stop' onClick={() => this.props.setTime(0)} noMarginRight />
+            </div>
+            <div style={[styles.customTimeControls, styles.container]}>
+              <h6 style={styles.controlsHeader}>Set custom time (in minutes)</h6>
+              <input id='custom-time-input' type='text' style={styles.input} placeholder='...' />
+              <Button onClick={this.setCustomTime} type='success' text='Set' />
+              <Button onClick={this.toggleCustomTimeControls} type='danger' text='Cancel' />
             </div>
             <div style={styles.container}>
               <h6 style={styles.controlsHeader}>Timer font size</h6>
-              <input type="range" min="100" max="200" value={this.props.fontSize} onChange={this.props.handleFontSizeChange} />
+              <input type='range' min='100' max='200' value={this.props.fontSize} onChange={this.props.handleFontSizeChange} />
             </div>
           </div>
         )
