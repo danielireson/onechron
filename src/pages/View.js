29005f22e0ae1de8b00c952d6390851db555fc3d
root <root@UoMISoc>
2020-11-23 08:14:19 +0000

    test

---
+++ src/pages/View.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { browserHistory } from 'react-router'
+
+import TimerStore from '../stores/TimerStore'
+
+import Clock from '../components/Clock'
+import Footer from '../components/Footer'
+import FullScreenButton from '../components/FullScreenButton'
+import Timer from '../components/Timer'
+import TimerLink from '../components/TimerLink'
+import TimerControls from '../components/TimerControls'
+
+class Live extends Component {
+  constructor() {
+    super()
+    TimerStore.resetTimer()
+  }
+
+  componentWillMount() {
+    this.checkTimerExists()
+  }
+
+  checkTimerExists() {
+    TimerStore.setPath(this.props.params.path)
+    TimerStore.checkForClearPath().then((isClearPath) => {
+      if (isClearPath) {
+        // No timer at this URL therefore redirect home
+        browserHistory.push('/')
+      }
+    })    
+  }
+
+  render() {
+    return (
+      <div>
+        <Clock />
+        <FullScreenButton />
+        <TimerLink />
+        <Timer />
+        <TimerControls />
+        <Footer />
+      </div>
+    )
+  }  
+}
+
+export default Live
